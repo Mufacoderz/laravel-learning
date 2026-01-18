12f84f36a719c9984ffb281d4ae14fdae6f58b0b
Mufacoderz <mf9555759@gmail.com>
2026-01-18 20:39:31 +0800

    strruktur flder

---
+++ resources/views/about.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>about</title>
+</head>
+<body>
+
+<h1>Halamn about</h1>
+
+{{-- ambil data nama dri routes --}}
+<h3>Owner: {{$nama}}</h3>
+
+<a href="/">welocme</a>
+<a href="/about">about</a>
+    <a href="/home">home</a>
+</body>
+</html>
